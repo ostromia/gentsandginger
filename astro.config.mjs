71f693e528ccc10c996698b90c57ef9f47831ff0
ostromia <66202981+ostromia@users.noreply.github.com>
2025-06-11 18:45:41 +0100

    refactor fonts using astro fonts api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
@@ -7,4 +7,24 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.fontsource(),
+                name: "Atkinson Hyperlegible",
+                cssVariable: "--font-atkinson-hyperlegible"
+            },
+            {
+                provider: fontProviders.fontsource(),
+                name: "Reenie Beanie",
+                cssVariable: "--font-reenie-beanie"
+            },
+            {
+                provider: fontProviders.fontsource(),
+                name: "Sedgwick Ave",
+                cssVariable: "--font-sedgwick-ave"
+            }
+        ]
+    }
 });
