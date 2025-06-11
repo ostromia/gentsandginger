517c1817929d650b597adfe0a78a1d03a12c0ad1
ostromia <66202981+ostromia@users.noreply.github.com>
2025-06-11 18:56:01 +0100

    prettier support

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+};
+
+// /**
+//  * @see https://prettier.io/docs/configuration
+//  * @type {import("prettier").Config}
+//  */
+// const config = {
+//     useTabs: false,
+//     tabWidth: 4,
+//     singleQuote: false,
+//     trailingComma: "none",
+//     printWidth: 100,
+//     plugins: ["@trivago/prettier-plugin-sort-imports"],
+//     importOrder: [
+//         "^(react|react-dom|react-dom/(client|server))$",
+//         "<THIRD_PARTY_MODULES>",
+//         "^[./]"
+//     ],
+//     importOrderSeparation: false
+// };
+
+// export default config;
