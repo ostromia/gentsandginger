45039fd9e707f54a23b00727b8828617105ff742
ostromia <66202981+ostromia@users.noreply.github.com>
2025-06-11 17:31:08 +0100

    contain events data within separate directory

---
+++ src/assets/events/index.ts
@@ -0,0 +1,40 @@
+import thefulfordarms from "./thefulfordarms.jpg";
+import truckfestival from "./truckfestival.jpeg";
+import bandonthewall from "./bandonthewall.jpg";
+import lumafilmtvfestival from "./lumafilmtvfestival.jpg"
+
+const events = [
+    {
+        title: "Truckfestival 2025",
+        date: "24 - 27 June 2025",
+        image: truckfestival.src,
+        url: "https://truckfestival.com/line-up/",
+        location: "Hill Farm, Oxfordshire"
+    },
+
+    {
+        title: "Luma Film Festival",
+        date: "9 - 11 June 2025",
+        image: lumafilmtvfestival.src,
+        url: "https://www.lumafilmfestival.co.uk",
+        location: "The School of Art, York"
+    },
+
+    {
+        title: "Gents & Ginger Reunion",
+        date: "8 June 2025",
+        image: thefulfordarms.src,
+        url: "https://www.thefulfordarms.com/event/gents-ginger/",
+        location: "The Fulford Arms, York"
+    },
+
+    {
+        title: "Band on the Wall",
+        date: "2 Auguest 2024",
+        image: bandonthewall.src,
+        url: "https://bandonthewall.org/events/elephunk-in-the-room-gents-and-gingers-the-palpitations/",
+        location: "Band on the Wall, Manchester"
+    },
+];
+
+export default events;
