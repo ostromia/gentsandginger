a8f3f2af0534a96911a002d1ba9005d5499536c3
ostromia <66202981+ostromia@users.noreply.github.com>
2025-06-12 15:46:51 +0100

    deploy script to automate hosting to github pages

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# options
+set -e
+
+shopt -s extglob dotglob
+
+# commands
+npm run build
+
+git switch build
+
+rm -rf !(dist|.astro|node_modules|.gitignore|.git)
+
+mv dist/* .
+
+rm -rf dist/
+
+git add --all
+
+git commit -m "$(date)"
+
+git push
+
+git switch main
