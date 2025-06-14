499860fb6eb8b7d482360349a7be59cc6e6fefe1
ostromia <66202981+ostromia@users.noreply.github.com>
2025-06-15 00:48:36 +0100

    temporary merchandise data

---
+++ src/assets/merchandise/index.ts
@@ -0,0 +1,36 @@
+import _0001 from "./0001.png";
+import _0002 from "./0002.png";
+import _0003 from "./0003.png";
+import _0004 from "./0004.png";
+
+const events = [
+    {
+        name: "Those Were The Days Tour Poster",
+        date: "01.01.2000",
+        image: _0001.src,
+        price: 100.0
+    },
+
+    {
+        name: "Gents & Ginger Beanie",
+        date: "01.01.2000",
+        image: _0002.src,
+        price: 100.0
+    },
+
+    {
+        name: "Gents & Ginger Reunion T-Shirt",
+        date: "01.01.2000",
+        image: _0003.src,
+        price: 100.0
+    },
+
+    {
+        name: "Gents & Ginger Frog T-Shirt",
+        date: "01.01.2000",
+        image: _0004.src,
+        price: 100.0
+    }
+];
+
+export default events;
